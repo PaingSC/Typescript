201616529f29ca5985a89962c9a38e5b296d6f86
PaingSC <paingsoechit.job@gmail.com>
2025-05-08 12:13:03 +0700

    Working with array

--- arrays.ts
+++ arrays.ts
@@ -21,3 +21,56 @@ type Point = {
 
 const coords: Point[] = [];
 coords.push({ x: 34, y: 22 });
+
+// type: Multidimensional Array
+// 2 dimensional array
+let matrix: string[][] = [[], [], []];
+matrix = [
+  ["2x", "3y", "z"],
+  ["4x", "2y", "2z"],
+  ["2x", "3y", "4z"],
+];
+
+// 3 dimensional array
+let tirMatrix: number[][][] = [[[23, 84, 9]]];
+
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Create an empty array of numbers called "ages":
+let ages: number[] = [];
+
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// Create an array variable called gameBoard that starts as an empty array.
+// It should be typed to hold a 2 dimensional array of strings
+let gameBoard: string[][] = [];
+
+// **********************************************
+// ******************* PART 3 *******************
+// **********************************************
+// Create a Product type that contains a name and a price.
+// An example product could be:
+// {name: "coffee mug", price: 11.50}
+
+type Product = {
+  name: string;
+  price: number;
+};
+
+const product: Product = {
+  name: "Coffee Mug",
+  price: 11.5,
+};
+
+// **********************************************
+// ******************* PART 4 *******************
+// **********************************************
+// Write a function called getTotal that accepts an array of Product types
+// It should return the sum of all the products' prices
+
+function getTotal(products: Product[]): number {
+  let sum = products.reduce((acc, cur) => acc + cur.price, 0);
+  return sum;
+}
