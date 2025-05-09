0c72c116980ac51ef593c4e79e2bedb7c2e4f736
PaingSC <paingsoechit.job@gmail.com>
2025-05-09 22:39:26 +0700

    Working with type: enum

--- tuplesEnums.ts
+++ tuplesEnums.ts
@@ -17,3 +17,32 @@ const responses: HTTPResponseTuple[] = [
   [200, "OK"],
   [500, "Internal server error"],
 ];
+
+// type: enum
+enum OrderStatus {
+  PENDING,
+  SHIPPED,
+  DELIEVERED,
+  RETURNED,
+}
+
+const curStatus = OrderStatus.DELIEVERED;
+
+function isDelivered(status: OrderStatus) {
+  return status === OrderStatus.DELIEVERED;
+}
+// Enum with strings
+enum ArrowKeys {
+  UP = "up",
+  DOWN = "down",
+  LEFT = "left",
+  RIGHT = "right",
+}
+
+// type: const enum
+const enum Orders {
+  PENDING,
+  SHIPPED,
+  DELIEVERED,
+  RETURNED,
+}
